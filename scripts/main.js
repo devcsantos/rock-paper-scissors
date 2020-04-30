1e62509e0f330518f71e4eddcf5a2b970113c2c0
Christian Santos <csantos1517@gmail.com>
2020-04-30 14:48:00 +0800

    animated choice to arena box

--- scripts/main.js
+++ scripts/main.js
@@ -11,8 +11,21 @@ let opponentChoice;
 playerChoice = ROCK;
 opponentChoice = SCISSORS;
 
-
+loadEventListeners();
 evaluate(playerChoice, opponentChoice);
+
+function loadEventListeners() {
+  let choices = document.getElementsByClassName('side-box');
+
+  for(let index=0; index < choices.length; index++) {
+    let side = choices[index].children;
+    for(let i=0;  i<side.length; i++) {
+      let choice = side[i];
+      //choice.addEventListener(onclick, translatePosition, false); wont work for some reason
+      choice.onclick = translatePosition;
+    }
+  }
+}
 
 function evaluate(playerChoice, opponentChoice) {
   let message;
@@ -34,4 +47,14 @@ function evaluate(playerChoice, opponentChoice) {
   console.log(message);
 }
 
+function translatePosition() {
+  let sideName = this.parentElement.id;
+  let arenaOffsetX = document.getElementById(`${sideName}-choice`).offsetLeft; // get the x position of arena
+  let arenaOffsetY = document.getElementById(`${sideName}-choice`).offsetTop; // get the y position of arena
 
+  let moveX = arenaOffsetX - this.offsetLeft; // amount of pixels to move in x-axis
+  let moveY = arenaOffsetY - this.offsetTop; // amount of pixels to move in y-axis
+
+  this.style.transform =  `translate(${moveX}px,${moveY}px)`;
+  console.log(`${sideName} click`);
+}
